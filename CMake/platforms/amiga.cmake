26caf76146f6806a7cbb04fc68cee483fa5ba45a
staphen <staphen@gmail.com>
2026-09-14 20:52:46 -0400

    Adjust workaround for fseeko detection on Amiga

--- CMake/platforms/amiga.cmake
+++ CMake/platforms/amiga.cmake
@@ -10,12 +10,16 @@ set(SDL1_VIDEO_MODE_BPP 8)
 set(DEVILUTIONX_SYSTEM_BZIP2 OFF)
 set(DEVILUTIONX_SYSTEM_ZLIB OFF)
 
-# `fseeko` fails to link on Amiga.
-add_definitions(-Dfseeko=fseek)
-
 list(APPEND DEVILUTIONX_PLATFORM_LINK_LIBRARIES ZLIB::ZLIB)
 if(NOT WARPOS)
   list(APPEND DEVILUTIONX_PLATFORM_LINK_LIBRARIES -ldebug)
 endif()
 
 file(COPY "${CMAKE_CURRENT_SOURCE_DIR}/Packaging/amiga/devilutionx.info" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}")
+
+# zlib uses check_function_exists() to determine if fseeko exists.
+# That doesn't work properly when CMAKE_TRY_COMPILE_TARGET_TYPE
+# is set to STATIC_LIBRARY, as it normally should be when cross-compiling.
+# Instead, call check_symbol_exists() here before zlib has a chance to get it wrong.
+include(CheckSymbolExists)
+check_symbol_exists(fseeko stdio.h HAVE_FSEEKO)
